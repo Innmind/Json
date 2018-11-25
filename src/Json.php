ab5650c0d56cc9fdc6b9f248dc61a95b5f1fafe8
Matthieu Napoli <matthieu@mnapoli.fr>
2018-11-25 13:45:18 +0100

    Add support for `json_encode()` options

--- src/Json.php
+++ src/Json.php
@@ -37,9 +37,9 @@ final class Json
     /**
      * @param mixed $content
      */
-    public static function encode($content): string
+    public static function encode($content, int $options = 0): string
     {
-        $json = json_encode($content);
+        $json = json_encode($content, $options);
 
         self::throwOnError();
 
